6ae510166342155f00d470e40707d3234837b373
isaactullis <irippletv@gmail.com>
2014-02-12 11:20:50 -0600

    Bigger
    Better

---
+++ number.rb
@@ -0,0 +1,11 @@
+puts 'What is your favorite number?'
+fnum = gets.chomp
+fnum2 = 1
+fnum3 = 2
+puts 'Wow! That is a cool number.'
+puts 'My favorite is'
+puts fnum.to_i + fnum2.to_i
+puts 'Because it is bigger. And we know bigger is better.' 
+puts 'I think you would like '
+puts fnum.to_i + fnum3.to_i
+puts 'The best because it is bigger. And we know bigger is better. Should I keep going? Or is your favorite number always bigger then it was before?'
